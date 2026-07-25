4ad261872285b272990801d8385bc1853495aa84
Christopher M O'Quinn <85000757+smithblack-0@users.noreply.github.com>
2026-07-25 13:47:46 -0700

    Export ClanController

--- src/clan_based_tuning/__init__.py
+++ src/clan_based_tuning/__init__.py
@@ -1,10 +1,6 @@
 """Clan Based Training: synchronous PBT over shared-gradient native trials."""
 
-from clan_based_tuning.controller import (
-    ClanPopulationPolicy,
-    MemberResult,
-    PopulationDecision,
-)
+from clan_based_tuning.controller import ClanController
 from clan_based_tuning.factory import (
     ClanLightningPlugins,
     make_clan_lightning_plugins,
@@ -20,12 +16,10 @@ from clan_based_tuning.ray import ClanBasedTraining, tune_checkpoint_path
 
 __all__ = [
     "ClanBasedTraining",
+    "ClanController",
     "ClanDDPStrategy",
     "ClanLightningEnvironment",
     "ClanLightningPlugins",
-    "ClanPopulationPolicy",
-    "MemberResult",
-    "PopulationDecision",
     "apply_optimizer_strategy",
     "make_clan_lightning_plugins",
     "prepare_clan_trainer",
