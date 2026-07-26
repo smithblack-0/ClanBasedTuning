07c65eb11c61721981b9c55795bfac6f1b902fb3
Christopher M O'Quinn <85000757+smithblack-0@users.noreply.github.com>
2026-07-26 14:28:26 -0700

    Skip Iris contract before importing Ray example

--- tests/framework_contracts/test_iris_scientific_workload.py
+++ tests/framework_contracts/test_iris_scientific_workload.py
@@ -5,9 +5,10 @@ import warnings
 
 import pytest
 
-from examples.iris_clan_experiment import REPORTS_PER_TRIAL, run_experiment
-
 pytestmark = [pytest.mark.framework_contract, pytest.mark.requires_ray]
+pytest.importorskip("ray")
+
+from examples.iris_clan_experiment import REPORTS_PER_TRIAL, run_experiment
 
 
 def test_initial_iris_workload_runs_through_public_manual_path(tmp_path):
