1ed880707d60c0032629102b73e0d5849eee289d
Christopher M O'Quinn <85000757+smithblack-0@users.noreply.github.com>
2026-07-26 13:43:25 -0700

    Export explicit Tune session

--- src/clan_based_tuning/ray/__init__.py
+++ src/clan_based_tuning/ray/__init__.py
@@ -2,5 +2,6 @@
 
 from clan_based_tuning.ray.checkpoint import tune_checkpoint_path
 from clan_based_tuning.ray.scheduler import ClanBasedTraining
+from clan_based_tuning.ray.session import ClanTuneSession
 
-__all__ = ["ClanBasedTraining", "tune_checkpoint_path"]
+__all__ = ["ClanBasedTraining", "ClanTuneSession", "tune_checkpoint_path"]
