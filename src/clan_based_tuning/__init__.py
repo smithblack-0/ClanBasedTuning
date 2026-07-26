0bb666d17c14141bb4bb18f05c8ae1bf7ec7771b
Christopher M O'Quinn <85000757+smithblack-0@users.noreply.github.com>
2026-07-25 17:55:01 -0700

    Export the Population data contract

--- src/clan_based_tuning/__init__.py
+++ src/clan_based_tuning/__init__.py
@@ -1,6 +1,6 @@
 """Clan Tuning policy and framework-integration research surfaces."""
 
-from clan_based_tuning.controller import ClanController
+from clan_based_tuning.controller import ClanController, Population
 from clan_based_tuning.factory import (
     ClanLightningPlugins,
     make_clan_lightning_plugins,
@@ -20,6 +20,7 @@ __all__ = [
     "ClanDDPStrategy",
     "ClanLightningEnvironment",
     "ClanLightningPlugins",
+    "Population",
     "apply_optimizer_strategy",
     "make_clan_lightning_plugins",
     "prepare_clan_trainer",
